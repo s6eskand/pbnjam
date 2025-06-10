19e21aa6d9b27e630290e385c32587f5adf69887
Sam Eskandar <sam.eskandar22@gmail.com>
2025-06-10 00:21:59 -0700

    input not output

--- src/audio.rs
+++ src/audio.rs
@@ -7,19 +7,19 @@ pub fn host_device_setup() -> Result<(cpal::Host, cpal::Device, cpal::SupportedS
     let host = cpal::default_host();
 
     let device = host
-        .default_output_device()
-        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
-    info!("Output device : {}", device.name()?);
+        .default_input_device()
+        .ok_or_else(|| anyhow::Error::msg("Default input device is not available"))?;
+    info!("Input device : {}", device.name()?);
 
-    let config = device.default_output_config()?;
-    info!("Default output config : {:?}", config);
+    let config = device.default_input_config()?;
+    info!("Default input config : {:?}", config);
 
     Ok((host, device, config))
 }
 
 pub fn make_stream<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<cpal::Stream, Error> where T: SizedSample + FromSample<f32> + std::fmt::Debug {
     let num_channels = config.channels as usize;
-    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+    let err_fn = |err| eprintln!("Error building input sound stream: {}", err);
 
     let stream = device.build_input_stream(
         config.into(),
