6fdee13d9e7ef6b633c462627021fa5eeee4cb3a
Sam Eskandar <sam.eskandar22@gmail.com>
2025-06-10 00:20:32 -0700

    get handler to input stream

---
+++ src/audio.rs
@@ -0,0 +1,43 @@
+use anyhow::{Result, Error};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Sample, FromSample, SizedSample};
+use tracing::{info, error};
+
+pub fn host_device_setup() -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
+    let host = cpal::default_host();
+
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
+    info!("Output device : {}", device.name()?);
+
+    let config = device.default_output_config()?;
+    info!("Default output config : {:?}", config);
+
+    Ok((host, device, config))
+}
+
+pub fn make_stream<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<cpal::Stream, Error> where T: SizedSample + FromSample<f32> + std::fmt::Debug {
+    let num_channels = config.channels as usize;
+    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+
+    let stream = device.build_input_stream(
+        config.into(),
+        move |input: &[T], _: &cpal::InputCallbackInfo| {
+            process_frame(input, num_channels);
+        },
+        err_fn,
+        None,
+    )?;
+
+    Ok(stream)
+}
+
+fn process_frame<SampleType>(
+    input: &[SampleType],
+    num_channels: usize,
+) where
+    SampleType: Sample + FromSample<f32> + std::fmt::Debug,
+{
+    println!("Input: {:?}", input);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use cpal::{traits::StreamTrait, I24};
+
+use pbnjam::audio::{make_stream, host_device_setup};
+
+fn main() -> anyhow::Result<()> {
+    let (_host, device, config) = host_device_setup()?;
+    let input_stream = match config.sample_format() {
+        cpal::SampleFormat::I8 => make_stream::<i8>(&device, &config.into()),
+        cpal::SampleFormat::I16 => make_stream::<i16>(&device, &config.into()),
+        cpal::SampleFormat::I24 => make_stream::<I24>(&device, &config.into()),
+        cpal::SampleFormat::I32 => make_stream::<i32>(&device, &config.into()),
+        cpal::SampleFormat::I64 => make_stream::<i64>(&device, &config.into()),
+        cpal::SampleFormat::U8 => make_stream::<u8>(&device, &config.into()),
+        cpal::SampleFormat::U16 => make_stream::<u16>(&device, &config.into()),
+        cpal::SampleFormat::U32 => make_stream::<u32>(&device, &config.into()),
+        cpal::SampleFormat::U64 => make_stream::<u64>(&device, &config.into()),
+        cpal::SampleFormat::F32 => make_stream::<f32>(&device, &config.into()),
+        cpal::SampleFormat::F64 => make_stream::<f64>(&device, &config.into()),
+        sample_format => Err(anyhow::Error::msg(format!(
+            "Unsupported sample format '{sample_format}'"
+        ))),
+    };
+
+    input_stream?.play()?;
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
+    Ok(())
 }
